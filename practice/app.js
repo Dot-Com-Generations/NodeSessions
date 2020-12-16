5ce757fd2205298156c21a73bd95027be4dfe72d
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-16 18:45:13 +0530

    init

---
+++ practice/app.js
@@ -0,0 +1,37 @@
+// ! Node Module System
+// ? 3 types of Modules
+// ? 1. Core modules - pre-installed with Node.
+// ? 2. Local Modules - User Made.
+// ? 3. npm Modules - 3rd Party Modules.
+
+
+// ? Installing Core Modules
+// const fs = require('fs')
+
+// fs.writeFileSync('test.txt', 'This is data')
+// fs.appendFileSync('test.txt', '\nThis data is being append!')
+
+// ? Importing Local Modules
+// console.log('This is app.js')
+// const { name, add } = require('./test.js')
+// console.log(name)
+// console.log(add(2, 5))
+
+// const sum = add(2, 5)
+// console.log(sum)
+
+// ? npm Modules
+
+// const validator = require('validator')
+
+// console.log(validator.isEmail('trehan@docogen.com'))
+// console.log(validator.isURL('https://docogen.com'))
+
+// const chalk = require('chalk');
+
+// if (validator.isEmail('trehan@docogen.com')) {
+//     console.log(chalk.blue.inverse('Email is Valid'))
+// }
+// else {
+//     console.log(chalk.red('Email is invalid'))
+// }
---
+++ practice/test.js
@@ -0,0 +1,11 @@
+// console.log('This is from test.js')
+// const name = 'Trehan'
+
+// const add = (a, b) => {
+//     return a + b
+// }
+
+// module.exports = {
+//     name,
+//     add
+// }
